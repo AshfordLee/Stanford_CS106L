2db59d78c6814d891e899edd88954f2dba6d3cab
AshfordLee <1207833942@qq.com>
2025-05-08 21:47:31 +0800

    Lec5

---
+++ Lec5/src/outputFileStreams.cpp
@@ -0,0 +1,19 @@
+#include<fstream>
+
+int main()
+{
+    std::ofstream ofs("hello.txt");
+
+    if (ofs.is_open())
+    {
+        ofs<<"Hello CS106L !"<<'\n';
+    }
+
+    ofs.close();
+    ofs<<"this will not get written";
+    
+    ofs.open("hello.txt");
+    ofs<<"this will though! It's open again";
+
+    return 0;
+}
---
+++ Lec5/src/stringstreamExampleBug.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+
+void foo()
+{
+    std::string initial_quote="Bjarne Stroustrup C makes it easy to shoot yourself in the foot";
+
+    std::stringstream ss(initial_quote);
+
+    std::string first;
+    std::string last;
+    std::string language,extracted_quote;
+
+    ss>>first>>last>>language;
+    std::cout << first << " " << last << " said this: \'" << language << " " << extracted_quote + "‘" << std::endl;
+}
+
+int main()
+{
+    foo();
+    return 0;
+}
